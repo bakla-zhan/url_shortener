bba674f6f60b1b5238422483ea011e432d3c89e8
Zhan <bakla_zhan@mail.ru>
2021-11-03 20:31:24 +0300

    changed for heroku rc2

--- db/sql/pg/pg.go
+++ db/sql/pg/pg.go
@@ -24,10 +24,10 @@ func NewStore(dsn string) (*Store, error) {
 	}
 
 	_, err = db.Exec(`create table IF NOT EXISTS links (short varchar(8), long varchar(1000));
-		create index IF NOT EXISTS concurrently links_short_idx on links using btree (short text_pattern_ops);
+		create index IF NOT EXISTS links_short_idx on links using btree (short text_pattern_ops);
 	
 		create table IF NOT EXISTS stats (link varchar(8), ip inet);
-		create index IF NOT EXISTS concurrently stats_link_ip_idx on stats using btree (link text_pattern_ops, ip inet_ops);`)
+		create index IF NOT EXISTS stats_link_ip_idx on stats using btree (link text_pattern_ops, ip inet_ops);`)
 	if err != nil {
 		db.Close()
 		return nil, err
